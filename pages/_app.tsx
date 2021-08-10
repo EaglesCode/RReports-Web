39e082016fe93c45a73a01b9e4e09f5a9c973f2c
Trifan Grigore-Gabriel <trifangaby71@protonmail.com>
2021-08-10 13:53:26 +0300

    🚑 Added Types

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import '../styles/globals.css'
+import { AppProps } from 'next/dist/next-server/lib/router/router';
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component , pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
 
